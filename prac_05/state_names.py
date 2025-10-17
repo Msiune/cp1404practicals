62468af681c65ee88827ac5f3ae6966f24bd42b2
masket <masket.siune@my.jcu.edu.au>
2025-10-17 14:52:04 +1000

    prac 5 walkthrough example End (5)

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,14 +5,36 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
-print(CODE_TO_NAME)
+ABBREVIATION_TO_STATES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory",
+                          "WA": "Western Australia",
+                          "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania",
+                          "SA": "South Australia"}
+# print(ABBREVIATION_TO_STATES)
+#
+# state_code = input("Enter short state: ").upper()
+# while state_code != "":
+#     if state_code in ABBREVIATION_TO_STATES:
+#         print(state_code, "is", ABBREVIATION_TO_STATES[state_code])
+#     else:
+#         print("Invalid short state")
+#     state_code = input("Enter short state: ").upper()
 
-state_code = input("Enter short state: ")
+
+
+abbreviation = list(ABBREVIATION_TO_STATES.keys())
+state_name = list(ABBREVIATION_TO_STATES.values())
+for i in range(len(ABBREVIATION_TO_STATES)):
+    print(f"{abbreviation[i]:3} is {state_name[i]}")
+
+
+print(ABBREVIATION_TO_STATES)
+
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    try:
+        print(state_code, "is", ABBREVIATION_TO_STATES[state_code])
+        state_code = input("Enter short state: ").upper()
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+        state_code = input("Enter short state: ").upper()
+
